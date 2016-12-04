63f59444550fd129c1f972bf5b142f9da110237b
pathFinder <path7inder@gmail.com>
2016-12-04 17:36:49 +0900

    add rest api function to shop controller
    api/shop
        - returns shopping list in json format
    
    api/shop?format=xml
        - returns shopping list in xml format

--- src/main/java/koreatech/cse/controller/blogandshop/ShopController.java
+++ src/main/java/koreatech/cse/controller/blogandshop/ShopController.java
@@ -3,6 +3,8 @@ package koreatech.cse.controller.blogandshop;
 import koreatech.cse.domain.blogandshop.daum.blog.DaumBlog;
 import koreatech.cse.domain.blogandshop.daum.shop.DaumShop;
 import koreatech.cse.domain.blogandshop.daum.shop.Item;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +25,32 @@ public class ShopController {
     private static String daum_blog_url = "https://apis.daum.net/search/blog";
 
     private static String daum_rest_api_key = "9cae039e021379f226d87b0f814c635b";   //본인 것으로 추가하기
+
+    private DaumShop getDaumShop(String searchWord) {
+        RestTemplate restTemplate = new RestTemplate();
+        DaumShop daumShop = null;
+        try {
+            ResponseEntity<DaumShop> daumShopResponseEntity
+                    = restTemplate.getForEntity(daum_shop_url + "?q=" + searchWord + "&output=json" + "&apikey=" + daum_rest_api_key, DaumShop.class);
+            daumShop = daumShopResponseEntity.getBody();
+            System.out.println(daumShop);
+        }catch (HttpClientErrorException e) {
+            System.out.println(e.getStatusCode() + ": " + e.getStatusText());
+        }
+        return daumShop;
+    }
+
+    @RequestMapping(value = "/api/shop", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<DaumShop> getShopWithJustAPIKey(@RequestParam(name = "searchWord") String searchWord) {
+        DaumShop daumShop = getDaumShop(searchWord);
+        return new ResponseEntity<DaumShop>(daumShop, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/api/shop", params = "format=xml", method = RequestMethod.GET, produces=MediaType.APPLICATION_XML_VALUE)
+    public ResponseEntity<DaumShop> getShopWithJustAPIKeyXML(@RequestParam(name = "searchWord") String searchWord) {
+        DaumShop daumShop = getDaumShop(searchWord);
+        return new ResponseEntity<DaumShop>(daumShop, HttpStatus.OK);
+    }
 
     @ApiIgnore
     @RequestMapping(value = "/shop_request", method = RequestMethod.GET)
