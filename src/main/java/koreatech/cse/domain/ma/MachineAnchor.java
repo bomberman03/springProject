b964210f4c94837783c19f7f9f86c5f14f595ac8
ujaki <ujakifox2@gmail.com>
2016-12-07 13:31:56 +0900

    Add new domain for Machine Anchor API
    — it contains a few properties and getters, setters

---
+++ src/main/java/koreatech/cse/domain/ma/MachineAnchor.java
@@ -0,0 +1,51 @@
+package koreatech.cse.domain.ma;
+
+/**
+ * Created by RyuSiHo on 2016. 12. 7..
+ */
+import com.fasterxml.jackson.annotation.*;
+import javax.annotation.Generated;
+
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+        "filename",
+        "download_link"
+})
+
+public class MachineAnchor {
+    @JsonProperty("filename")
+    private String filename;
+
+    @JsonProperty("download_link")
+    private String download_link;
+
+    @JsonProperty("filename")
+    public String getFilename() {
+        return filename;
+    }
+
+    @JsonProperty("filename")
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    @JsonProperty("download_link")
+    public String getDownload_link() {
+        return download_link;
+    }
+
+    @JsonProperty("download_link")
+    public void setDownload_link(String download_link) {
+        this.download_link = download_link;
+    }
+
+    @Override
+    public String toString() {
+        return "MachineAnchor{" +
+                "filename='" + filename + '\'' +
+                ", download_link='" + download_link + '\'' +
+                '}';
+    }
+}
